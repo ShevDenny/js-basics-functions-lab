c25efefbd8b2ac8b8665993c3e599885210b07ad
ShevDenny <sdenny904@gmail.com>
2021-05-19 13:02:24 -0400

    commit tree fd66a5209349c6987da95aa0d41be539d081183e

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block > 42) {
+        return block- 42;
+    } else {
+        return 42- block;
+    }
+}
+
+function distanceFromHqInFeet(block) {
+        return distanceFromHqInBlocks(block)* 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (start > end) {
+        return (start - end) * 264;
+    } else {
+        return (end - start) * 264;
+    }
+    
+}
+
+function calculatesFarePrice(start, end) {
+    if(distanceTravelledInFeet(start, end) <= 400) {
+        return 0
+    } else if(distanceTravelledInFeet(start, end) <=400 || distanceTravelledInFeet(start, end)<= 2000) {
+        return (528-400) * 2/100
+    } else if(distanceTravelledInFeet(start, end) <= 2000 || distanceTravelledInFeet(start, end) <= 2500) {
+        return 25
+    } else (distanceFromHqInFeet(start, end) <= 2500) 
+        return 'cannot travel that far'
+}
